30b110da6ae0b2249704cbfa81107d4b98c06e5c
Luciano Costa <lcosta_96@hotmail.com.br>
2018-04-04 13:33:55 -0300

    PyLama refactor for IFMA spider

--- edital_scrapper/spiders/ifma_spider.py
+++ edital_scrapper/spiders/ifma_spider.py
@@ -1,15 +1,23 @@
+"""IFMA official edict spider."""
 import scrapy
 
 
 class IfmaSpider(scrapy.Spider):
+
+    """IFMA spider class."""
+
     name = 'ifma'
 
     def start_requests(self):
+        """Start request method."""
         url = 'https://portal.ifma.edu.br/concursos-e-seletivos/'
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        editais = response.css('div.tile-list-1 div.video-item.row-fluid div.tileItem')
+        """Parse response method."""
+        editais = response.css(
+            'div.tile-list-1 div.video-item.row-fluid div.tileItem'
+        )
 
         for edital in editais:
             attachment_block = edital \
@@ -20,20 +28,25 @@ class IfmaSpider(scrapy.Spider):
             items = attachment_block.css('a::text').extract()[1::2]
 
             for i in range(len(attachment_block.css('a::attr(href)'))):
-                attachments[i] = dict(
-                    item=items[i],
-                    link=response.url+attachment_block.css('a::attr(href)').extract()[i]
-                )
-
+                attachments[i] = {
+                    'item': items[i],
+                    'link': response.url+attachment_block.css('a::attr(href)')
+                                                         .extract()[i]
+                }
 
             yield {
-                'title': edital.css('div.tileHeader h2 a::text').extract_first(),
+                'title': edital.css('div.tileHeader h2 a::text')
+                               .extract_first(),
                 'description': edital.css('span::text').extract_first(),
                 'attachments': attachments,
                 'information': {
-                    'date': edital.css('div.tileInfo ul li::text').extract()[0],
-                    'type': edital.css('div.tileInfo ul li::text').extract()[1],
-                    'campus': edital.css('div.tileInfo ul li::text').extract()[2],
-                    'situation': edital.css('div.tileInfo ul li span::text').extract_first()
+                    'date': edital.css('div.tileInfo ul li::text')
+                                  .extract()[0],
+                    'type': edital.css('div.tileInfo ul li::text')
+                                  .extract()[1],
+                    'campus': edital.css('div.tileInfo ul li::text')
+                                    .extract()[2],
+                    'situation': edital.css('div.tileInfo ul li span::text')
+                                       .extract_first()
                 }
             }
